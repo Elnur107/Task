d1a4f4b02be441687f5bcc66feae8cf99895f7e0
AkberliElnur <126491959+Elnur107@users.noreply.github.com>
2023-12-15 08:22:39 +0300

    Add files via upload

---
+++ Task/Task/Program.cs
@@ -0,0 +1,22 @@
+﻿(string username, string password) user = ("admin", "123");
+
+        while (true)
+        {
+            Console.Write("İstifadeci adi: ");
+            string enteredUsername = Console.ReadLine();
+
+            Console.Write("Şifrə: ");
+            string enteredPassword = Console.ReadLine();
+
+            if (enteredUsername == user.username && enteredPassword == user.password)
+            {
+                Console.WriteLine("Daxil oldunuz!");
+                break; 
+            }
+            else
+            {
+                Console.WriteLine("Yanlış istifadəçi adı və ya şifrə. Xahiş edirik yenidən cəhd edin.");
+            }
+        }
+    }
+}
